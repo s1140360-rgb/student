bcd7273bff8cb496d3e2ae03c128827d08eaf17f
s1140360-rgb <s1140360@dmhs.kh.edu.tw>
2025-11-07 11:36:33 +0800

    Add files via upload

---
+++ hw3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"快樂星球" << std::endl;
+    std::cout<<"快 樂 星 球" << std::endl;
+    std::cout<<"快\t樂\t星\t球" << std::endl;
+    std::cout<<"快\n樂\n星\n球" << std::endl;
+    std::cout<<"\"快樂星球\"" << std::endl;
+    std::cout<<"\\快樂星球\\" << std::endl;
+    
+
+    return 0;
+}
